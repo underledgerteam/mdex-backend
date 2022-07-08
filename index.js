94925dacd9e5146e5df0b355ee24db2cd2ffd2b9
littletanggg <wichitaserchuit@gmail.com>
2022-07-08 11:22:11 +0700

    :triangular_flag_on_post: Update import lib for best path swap

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
-const updateStateTransaction = require("./services/cron-jobs");
+const config = require('./configs/app')
+
+// Cron job
 require("./services/cron-jobs");
 
-app.listen(9000, () => {
-  console.log(`Server start on port: 3000`);
-});
+// Express Configs
+require('./configs/express')(app)
+
+// Routes
+app.use(require('./routes'))
+
+// Start Server
+const server = app.listen( config.port, () => {
+  let port = server.address().port
+  console.log(`Cron-job and API server start on port: ${port}`);
+})
