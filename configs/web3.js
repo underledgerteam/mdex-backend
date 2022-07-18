8ff905a9c6b3479ab019d66950417ab5e7d918e0
littletanggg <wichitaserchuit@gmail.com>
2022-07-18 17:37:46 +0700

    :fire: Delete uniswap abi and configs

--- configs/web3.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    defaultAccountAddress: process.env.DEFAULT_ACCOUNT_ADDRESS,
-    rinkebyHttpProvider: process.env.RINKEBY_HTTP_PROVIDER,
-    privateKey: process.env.PRIVATE_KEY,
-    ethConfirmations: 10
-}
--- routes/api/swap.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const controllers = require('../../controllers/swap.controllers')
-
-router.get('/', controllers.getText)
-router.get('/qoute', controllers.qoute)
-
-module.exports = router
