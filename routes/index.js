273e9e3dab821fe4b9520dde73a447e62b5a2257
littletanggg <wichitaserchuit@gmail.com>
2022-07-18 17:39:48 +0700

    :wrench: Remove sub routing api

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
-const config = require('../configs/app')
 
-router.use(`/api/v${config.apiVersion}`, require('./api'))
+router.use(`/api/`, require('./api'))
 
 module.exports = router
