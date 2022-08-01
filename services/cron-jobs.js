c16f23632bfa9faff39f148be1238fd96e822e3f
littletanggg <wichitaserchuit@gmail.com>
2022-08-01 17:03:38 +0700

    :triangular_flag_on_post: Add cronjob for Goerli network

--- services/cron-jobs.js
+++ services/cron-jobs.js
@@ -1,18 +1,28 @@
 const cron = require("node-cron");
 const ethers = require("ethers");
 const ABI = require("../abis/multisig-abi.json");
+const { ROUTING_CONTRACTS } = require("../utils/constants");
+
 require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 cron.schedule("* * * * *", async function () {
-  const CONTRACT_ADDRESS = "0xb236416f41e82884F6311065ad00b2F853dF436F";
-  const provider = ethers.getDefaultProvider(ethers.providers.getNetwork(4));
-  const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+  //Rinkeby
+  const rinkebyProvider = ethers.getDefaultProvider(ethers.providers.getNetwork(4));
+  const rinkebWallet = new ethers.Wallet(PRIVATE_KEY, rinkebyProvider);
+  const rinkebContract = new ethers.Contract(ROUTING_CONTRACTS[4].MultisigWallet, ABI, rinkebWallet);
 
-  const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet);
+  await rinkebContract.uppdateData();
 
-  await contract.uppdateData();
+  console.log("Run Cron Job Rinkeby Network: ", PRIVATE_KEY);
 
-  console.log("Run Cron Job", PRIVATE_KEY);
+  //Goerli
+  const goerliProvider = ethers.getDefaultProvider(ethers.providers.getNetwork(5));
+  const goerliWallet = new ethers.Wallet(PRIVATE_KEY, goerliProvider);
+  const goerliContract = new ethers.Contract(ROUTING_CONTRACTS[5].MultisigWallet, ABI, goerliWallet);
+
+  await goerliContract.uppdateData();
+
+  console.log("Run Cron Job Goerli Network: ", PRIVATE_KEY);
 });
--- utils/constants.js
+++ utils/constants.js
@@ -21,7 +21,7 @@ module.exports = {
 			"fee": 0.04
 		}
 	},
-
+	
 	ROUTING_CONTRACTS: {
 		4: {
 			Lable: "Rinkeby Testnet Network",
@@ -29,6 +29,7 @@ module.exports = {
 			ABIController: controllerABI,
 			ABIBestRouteQuery: bestRouteQueryABI,
 
+			MultisigWallet: "0x6F61d079A4C6678002E849532a2779D22c8230E8",
 			AddressController: "0x8207ef2260e98b5Ae3aF0419c22c5a76e9267De2",
 			AddressBestRouteQuery: "0x92CA3294eB72b212e53Eb4b900d0D691f9cd4F4d"
 		},
@@ -38,6 +39,7 @@ module.exports = {
 			ABIController: controllerABI,
 			ABIBestRouteQuery: bestRouteQueryABI,
 
+			MultisigWallet: "0xC91607721DE4297aF7a7c32BD658d0A963C8FC78",
 			AddressController: "0xe2e0DfA2dC80d847F6B6B9D67FE0fDa07B10EE5a",
 			AddressBestRouteQuery: "0xd19C6F58B9D06C0C3198993Ee9C34C08BA57195e"	
 		}
