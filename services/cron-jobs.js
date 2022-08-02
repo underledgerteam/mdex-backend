f1223be889d57bc6521920942f0be687a5fba482
Paramet Kongjaroen <parametkongjaroen@Prame-2.local>
2022-08-02 12:06:04 +0700

    :triangular_flag_on_post: update cron job

--- services/cron-jobs.js
+++ services/cron-jobs.js
@@ -7,22 +7,83 @@ require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-cron.schedule("* * * * *", async function () {
+cron.schedule("5 * * * * *", async function () {
   //Rinkeby
-  const rinkebyProvider = ethers.getDefaultProvider(ethers.providers.getNetwork(4));
-  const rinkebWallet = new ethers.Wallet(PRIVATE_KEY, rinkebyProvider);
-  const rinkebContract = new ethers.Contract(ROUTING_CONTRACTS[4].MultisigWallet, ABI, rinkebWallet);
+  const rinkebyProvider = ethers.getDefaultProvider(
+    ethers.providers.getNetwork(4)
+  );
+  const rinkebyWallet = new ethers.Wallet(PRIVATE_KEY, rinkebyProvider);
+  const rinkebyContract = new ethers.Contract(
+    ROUTING_CONTRACTS[4].MultisigWallet,
+    ABI,
+    rinkebyWallet
+  );
+  const transactionStateQueue =
+    await rinkebyContract.getTransactionStatusQueue();
 
-  await rinkebContract.uppdateData();
+  const blockNumber = await rinkebyProvider.getBlockNumber();
+  const blockTimestamp = await rinkebyProvider.getBlock(blockNumber);
+
+  if (transactionStateQueue.length > 0) {
+    for (let i = 0; i <= transactionStateQueue.length; i++) {
+      let convertBlockTimeStamp = ethers.BigNumber.from(
+        transactionStateQueue[i].timestamp
+      ).toNumber();
+
+      if (
+        transactionStateQueue[i].status == 2 &&
+        convertBlockTimeStamp <= blockTimestamp.timestamp
+      ) {
+        await rinkebyContract.updateTransaction(
+          ethers.BigNumber.from(transactionStateQueue[i].id).toNumber()
+        );
+      } else {
+        return true;
+      }
+    }
+  } else {
+    return true;
+  }
 
   console.log("Run Cron Job Rinkeby Network: ", PRIVATE_KEY);
 
   //Goerli
-  const goerliProvider = ethers.getDefaultProvider(ethers.providers.getNetwork(5));
+  const goerliProvider = ethers.getDefaultProvider(
+    ethers.providers.getNetwork(5)
+  );
   const goerliWallet = new ethers.Wallet(PRIVATE_KEY, goerliProvider);
-  const goerliContract = new ethers.Contract(ROUTING_CONTRACTS[5].MultisigWallet, ABI, goerliWallet);
+  const goerliContract = new ethers.Contract(
+    ROUTING_CONTRACTS[5].MultisigWallet,
+    ABI,
+    goerliWallet
+  );
 
-  await goerliContract.uppdateData();
+  const transactionStateQueue =
+    await goerliContract.getTransactionStatusQueue();
+
+  const blockNumber = await rinkebyProvider.getBlockNumber();
+  const blockTimestamp = await rinkebyProvider.getBlock(blockNumber);
+
+  if (transactionStateQueue.length > 0) {
+    for (let i = 0; i <= transactionStateQueue.length; i++) {
+      let convertBlockTimeStamp = ethers.BigNumber.from(
+        transactionStateQueue[i].timestamp
+      ).toNumber();
+
+      if (
+        transactionStateQueue[i].status == 2 &&
+        convertBlockTimeStamp <= blockTimestamp.timestamp
+      ) {
+        await goerliContract.updateTransaction(
+          ethers.BigNumber.from(transactionStateQueue[i].id).toNumber()
+        );
+      } else {
+        return true;
+      }
+    }
+  } else {
+    return true;
+  }
 
   console.log("Run Cron Job Goerli Network: ", PRIVATE_KEY);
 });
--- utils/constants.js
+++ utils/constants.js
@@ -2,46 +2,46 @@ const controllerABI = require("../abis/mdexControllerABI.json");
 const bestRouteQueryABI = require("../abis/mdexBestRouteQueryABI.json");
 
 module.exports = {
-	DISTRIBUTION_PERCENT: 5,
-	SWAP_FEE: 1,
-	ROUTES: [0, 1],
+  DISTRIBUTION_PERCENT: 5,
+  SWAP_FEE: 1,
+  ROUTES: [0, 1],
 
-	ROUTING_NAME: {
-		0: "Uniswap",
-		1: "Curve Fi."
-	},
+  ROUTING_NAME: {
+    0: "Uniswap",
+    1: "Curve Fi.",
+  },
 
-	DEX: {
-		0: {
-			"name": "Uniswap",
-			"fee": 0.3
-		},
-		1: {
-			"name": "Curve Fi.",
-			"fee": 0.04
-		}
-	},
-	
-	ROUTING_CONTRACTS: {
-		4: {
-			Lable: "Rinkeby Testnet Network",
+  DEX: {
+    0: {
+      name: "Uniswap",
+      fee: 0.3,
+    },
+    1: {
+      name: "Curve Fi.",
+      fee: 0.04,
+    },
+  },
 
-			ABIController: controllerABI,
-			ABIBestRouteQuery: bestRouteQueryABI,
+  ROUTING_CONTRACTS: {
+    4: {
+      Lable: "Rinkeby Testnet Network",
 
-			MultisigWallet: "0x6F61d079A4C6678002E849532a2779D22c8230E8",
-			AddressController: "0x8207ef2260e98b5Ae3aF0419c22c5a76e9267De2",
-			AddressBestRouteQuery: "0x92CA3294eB72b212e53Eb4b900d0D691f9cd4F4d"
-		},
-		5: {
-			Lable: "Goerli Testnet Network",
+      ABIController: controllerABI,
+      ABIBestRouteQuery: bestRouteQueryABI,
 
-			ABIController: controllerABI,
-			ABIBestRouteQuery: bestRouteQueryABI,
+      MultisigWallet: "0x2A5e8342EEcD3DCD22D4720A6f3B7dDFCA129868",
+      AddressController: "0x8207ef2260e98b5Ae3aF0419c22c5a76e9267De2",
+      AddressBestRouteQuery: "0x92CA3294eB72b212e53Eb4b900d0D691f9cd4F4d",
+    },
+    5: {
+      Lable: "Goerli Testnet Network",
 
-			MultisigWallet: "0xC91607721DE4297aF7a7c32BD658d0A963C8FC78",
-			AddressController: "0xe2e0DfA2dC80d847F6B6B9D67FE0fDa07B10EE5a",
-			AddressBestRouteQuery: "0xd19C6F58B9D06C0C3198993Ee9C34C08BA57195e"	
-		}
-	}
-}
+      ABIController: controllerABI,
+      ABIBestRouteQuery: bestRouteQueryABI,
+
+      MultisigWallet: "0x5D9b61B62D27E310FE8679a76d27a558bD0E016D",
+      AddressController: "0xe2e0DfA2dC80d847F6B6B9D67FE0fDa07B10EE5a",
+      AddressBestRouteQuery: "0xd19C6F58B9D06C0C3198993Ee9C34C08BA57195e",
+    },
+  },
+};
