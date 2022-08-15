720baafa15bf00dbef719ced11f7a233bcea46be
littletanggg <wichitaserchuit@gmail.com>
2022-08-15 16:32:00 +0700

    :fire: Remove no need routes folder

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const controllers = require('../../controllers/swap.controllers')
-
-router.get('/rate', controllers.getBestRate);
-router.post('/swap', controllers.swap);
-
-module.exports = router
