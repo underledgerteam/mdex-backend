0bdee04604aa8ba98dea62e05b65c6fed2bfeb03
littletanggg <wichitaserchuit@gmail.com>
2022-08-23 14:05:34 +0700

    :fire: Remove no need env variable

--- configs/app.js
+++ configs/app.js
@@ -5,6 +5,5 @@ module.exports = {
   port: process.env.PORT || 9000,
   isProduction: process.env.NODE_ENV === 'production',
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'my-secret',
-  apiKey: process.env.API_KEY || 'supersecret',
-  apiVersion: process.env.API_VERSION || 1
+  apiKey: process.env.API_KEY || 'supersecret'
 }
